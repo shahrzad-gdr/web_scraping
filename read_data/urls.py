b2ed9f56f934d0a1edfbc5b53693ffd3887f4466
Shahrzad Gdr <goudarzi.shahrzad825@gmail.com>
2023-04-18 17:55:09 +0330

    add read_movies path

--- read_data/urls.py
+++ read_data/urls.py
@@ -5,5 +5,6 @@ from read_data import views
 urlpatterns = [
     path('', views.home, name= 'index'),
     path('extract/', views.extract_data, name= 'extract_data'),
+    path('read/', views.read_movies, name= 'read_movies'),
 
 ]
