7f5fa22e3d6a3070d423c0f3f3ed37485b10e5b2
Shahrzad Gdr <goudarzi.shahrzad825@gmail.com>
2023-04-18 15:55:44 +0330

    define a model which stores movie information

--- read_data/models.py
+++ read_data/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
+class Moive(models.Model):
+
+    title = models.CharField(max_length=150)
+    rank  = models.DecimalField(max_digits=3, decimal_places=1)
+    year  = models.IntegerField()
+    director = models.CharField(max_length=50)
+    link = models.URLField()
+    poster = models.URLField()
+
+
+    def __str__(self):
+        return self.title
+
+
 
 
